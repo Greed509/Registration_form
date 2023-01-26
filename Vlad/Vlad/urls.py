41e1ae916ae14d4b2058374fbd9dbed9296a0f3c
Greed509 <greed509@gmail.com>
2023-01-26 20:14:45 +0500

    26.01.23

--- Vlad/Vlad/urls.py
+++ Vlad/Vlad/urls.py
@@ -2,6 +2,22 @@ from django.urls import path
 from regi import views
   
 urlpatterns = [
-    path("", views.index),
-    path("registration", views.registration),
+    path("", views.sign),
+    path("register", views.registration),
+    path("glavnaya", views.glavnaya),
+    path("strim", views.strim),
+    path("gif", views.gif),
+    path("video", views.video),
+    path("strim1", views.strim1),
+    path("strim2", views.strim2),
+    path("strim3", views.strim3),
+    path("strim4", views.strim4),
+    path("strim5", views.strim5),
+    path("strim6", views.strim6),
+    path("video1", views.video1), 
+    path("video2", views.video2), 
+    path("video3", views.video3), 
+    path("video4", views.video4), 
+    path("video5", views.video5), 
+    path("video6", views.video6),     
 ]
--- Vlad/regi/views.py
+++ Vlad/regi/views.py
@@ -6,19 +6,18 @@ my_file = open("Base.txt", "r")
 base = my_file.read()
 lst = base.split()
 
-def index(request):
+def sign(request):
     if request.method == "POST":
         login = request.POST.get("Логин")
         password = request.POST.get("Пароль")
         user_check = login + password
         if user_check in lst:                              
-            return HttpResponse(f"<h2>Привет, {login}</h2>")
-                #Добавить
+            return render(request, "glavnaya.html")
         else:
             return HttpResponse(f"<h2>Неверный логин или пароль</h2>")
     else:
         userform = UserForm()
-        return render(request, "index.html", {"form": userform})
+        return render(request, "sign.html", {"form": userform})
 
 def registration(request):
     if request.method == "POST":
@@ -35,4 +34,58 @@ def registration(request):
             return HttpResponse(f"<h2>Пользователь {login} уже, существует!</h2>")      
     else:
         userform = RegForm()        
-        return render(request, "registration.html", {"form": userform})
+        return render(request, "register.html", {"form": userform})
+
+def glavnaya(request):
+    return render(request, "glavnaya.html")
+
+def strim(request):
+    return render(request, "Strim.html")
+
+def gif(request):
+    return render(request, "gif.html")
+
+def video(request):
+    return render(request, "video.html")
+
+def strim1(request):
+    return render(request, "strim1.html")
+
+def strim2(request):
+    return render(request, "strim2.html")
+
+def strim3(request):
+    return render(request, "strim3.html")
+
+def strim4(request):
+    return render(request, "strim4.html")
+
+def strim5(request):
+    return render(request, "strim5.html")
+
+def strim6(request):
+    return render(request, "strim6.html")
+
+def video1(request):
+    return render(request, "video1.html")
+
+def video2(request):
+    return render(request, "video2.html")
+
+def video3(request):
+    return render(request, "video3.html")
+
+def video4(request):
+    return render(request, "video4.html")
+
+def video5(request):
+    return render(request, "video5.html")
+
+def video6(request):
+    return render(request, "video6.html")
+
+
+
+
+
+
